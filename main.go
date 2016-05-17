f527b811f6a232f53928ade760e935ee03e91859
MendelGusmao <mendelsongusmao@gmail.com>
2016-05-17 11:45:17 -0300

    Simplifying

--- main.go
+++ main.go
@@ -16,13 +16,21 @@ From https://support.google.com/mail/answer/10313
 	changing the actual destination address; they'll all go to
 	your inbox, and only yours."
 
+	This piece of code provides a deterministic way of generating different e-mail addresses
+	from an e-mail address by putting dots between its characters. So, after receiving an spam
+	sent to one of your "new" addresses, you can track and have the information of what service
+	forwarded your address.
+
+	It's just a proof of concept and should not be used seriously. Some smart spammers might just
+	remove the dots (and/or the plus sign and what comes after) from the addresses they receive.
+
 	Usage: dotnator <email address> <service name or address> [salt]
 
 */
 
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Println("Usage:\n\tdotinator <email address> <service name or address> [salt]")
+		fmt.Println("Usage:\n\tdotnator <email address> <service name or address> [salt]")
 		os.Exit(1)
 	}
 
@@ -39,19 +47,18 @@ func main() {
 
 	if i := strings.Index(username, "+"); i > -1 {
 		plus = username[i:]
-		username = username[0:i]
+		username = username[:i]
 	}
 
 	key := append([]byte(salt), []byte(service)...)
 	crc := crc64.Checksum(key, crc64.MakeTable(crc64.ECMA))
 	crcp := fmt.Sprintf("%063s", strconv.FormatInt(int64(crc), 2))
 	index := int(service[0]) % (63 - len(username))
-	part := crcp[index : index+len(username)-1]
 	dots := make(map[int]bool)
 
-	for i := range part {
-		if part[i] == '1' {
-			dots[i] = true
+	for i := index; i < index+len(username)-1; i++ {
+		if crcp[i] == '1' {
+			dots[i-index] = true
 		}
 	}
 
